ae8a1299b5b51e03bbbdf6d9ac0b2fbd55efe52f
hidoshino <91507697+FilipePaiva10@users.noreply.github.com>
2021-10-12 06:56:27 -0300

    Primeiras Confiugurações

---
+++ src/app.styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+
+`;
+
+export const Header = styled.div`
+
+`;
+
+export const HeaderText = styled.h1`
+
+
+`;
+
+export const Body = styled.div`
+
+`;
